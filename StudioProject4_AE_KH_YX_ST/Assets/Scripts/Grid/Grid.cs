f8cbffccf16b665cee82f7500702c2c3ee8a8d26
darth343 <darth343@gmail.com>
2017-02-14 15:46:43 +0800

    Changed GridMesh Codes to be able to switch game object to show what availability state its in

--- StudioProject4_AE_KH_YX_ST/Assets/Scripts/Grid/Grid.cs
+++ StudioProject4_AE_KH_YX_ST/Assets/Scripts/Grid/Grid.cs
@@ -3,29 +3,60 @@ using System.Collections;
 
 public class Grid : MonoBehaviour
 {
-    public Texture2D Avail;
-    public Texture2D Unavail;
+    public GameObject Avail;
+    public GameObject UnAvail;
     public bool isAvailable = true;
     public Vector2 position;
 
+    public void GenerateMeshes()
+    {
+        Avail.transform.localPosition = new Vector3(0, 0, 0);
+        Avail.transform.localScale = new Vector3(1, 1, 1);
+        Avail.transform.SetParent(gameObject.transform);
+
+        UnAvail.transform.localPosition = new Vector3(0, 0, 0);
+        UnAvail.transform.localScale = new Vector3(1, 1, 1);
+        UnAvail.transform.SetParent(gameObject.transform);
+    }
+
+    public Vector3 GetWorldPosition()
+    {
+        return new Vector3(position.x * gameObject.transform.parent.GetComponent<GridArray>().GridSizeX + gameObject.transform.parent.GetComponent<GridArray>().GridSizeX * 0.5f, 0, position.y * gameObject.transform.parent.GetComponent<GridArray>().GridSizeZ + gameObject.transform.parent.GetComponent<GridArray>().GridSizeZ * 0.5f);
+    }
+
+    public bool CollidedWithTerrain()
+    {
+        Terrain ground = gameObject.transform.parent.GetComponent<GridArray>().ground;
+        Vector3 minPos = GetWorldPosition() - (new Vector3(gameObject.transform.parent.GetComponent<GridArray>().GridSizeX * 0.5f, 0, gameObject.transform.parent.GetComponent<GridArray>().GridSizeZ * 0.5f));
+        Vector3 maxPos = GetWorldPosition() + (new Vector3(gameObject.transform.parent.GetComponent<GridArray>().GridSizeX * 0.5f, 0, gameObject.transform.parent.GetComponent<GridArray>().GridSizeZ * 0.5f));
+
+        if (0.05 < ground.SampleHeight(minPos) && 0.05 < ground.SampleHeight(maxPos))
+        {
+            return true;
+        }
+        return false;
+    }
+
     public void UpdateAvailability()
     {
-        if (0.05 < transform.parent.GetComponent<GenerateGrid>().ground.SampleHeight(new Vector3(position.x * transform.parent.GetComponent<GenerateGrid>().GridSizeX, 0, position.y * transform.parent.GetComponent<GenerateGrid>().GridSizeZ)))
-        {
-            isAvailable = false;
-        }
-        if (!isAvailable)
-            GetComponent<Renderer>().material.SetTexture("_MainTex", Unavail);
-        else
-            GetComponent<Renderer>().material.SetTexture("_MainTex", Avail);
+        isAvailable = !CollidedWithTerrain();
 
-        //GetComponent<Renderer>().material.SetFloat("_Mode", 1);
+        if (isAvailable)
+        {
+            Avail.SetActive(true);
+            UnAvail.SetActive(false);
+        }
+        else
+        {
+            Avail.SetActive(false);
+            UnAvail.SetActive(true);
+        }
     }
 
 	// Use this for initialization
 	void Start () 
     {
-        //UpdateAvailability();
+        GenerateMeshes();
 	}
 	
 	// Update is called once per frame
---
+++ StudioProject4_AE_KH_YX_ST/Assets/Scripts/Grid/GridArray.cs
@@ -0,0 +1,58 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GridArray : MonoBehaviour
+{
+    public GameObject StartingGrid;
+    public Terrain ground;
+    public int GridSizeX = 10;
+    public int GridSizeZ = 10;
+    public bool isGenerated = false;
+    public int m_rows;
+    public int m_columns;
+    public GameObject[,] gridmesh;
+
+	// Use this for initialization
+	void Start ()
+    {
+        GenerateGrid();	
+	}
+
+    GameObject GetGridAtPosition(Vector3 position)
+    {
+        return null;
+    }
+
+    void GenerateGrid()
+    {
+        if (isGenerated)
+            return;
+        int m_rows = (int)ground.terrainData.size.x / GridSizeX;
+        int m_columns = (int)ground.terrainData.size.z / GridSizeZ;
+        gridmesh = new GameObject[m_rows, m_columns]; // hardcoded, not sure if const var can be changed in editor , change this if you don't want to hardcode Arun
+        Debug.Log("run");
+        // Create rows
+        for (int x = 0; x < m_rows; x++)
+        { // Create columns
+            for (int z = 0; z < m_columns; z++)
+            {
+                // Create a copy of the plane and offset it according to [current width, current column] using Instantiate
+                GameObject grid = (GameObject)Instantiate(StartingGrid);
+                grid.name = "Row: " + x + " Col: " + z;
+                grid.transform.position = new Vector3(x * GridSizeX + GridSizeX * 0.5f, 0.8f, z * GridSizeZ + GridSizeZ * 0.5f);
+                grid.transform.localScale = new Vector3(GridSizeX, 1, GridSizeZ);
+                grid.transform.SetParent(gameObject.transform);
+                grid.GetComponent<Grid>().position.x = x;
+                grid.GetComponent<Grid>().position.y = z;
+                grid.GetComponent<Grid>().UpdateAvailability();
+                //, new Vector3(m_startingPlane.transform.position.x + x, m_startingPlane.transform.position.y, m_startingPlane.transform.position.z + z), m_startingPlane.transform.rotation);
+                gridmesh[x, z] = grid;
+            }
+        }
+    }
+
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
