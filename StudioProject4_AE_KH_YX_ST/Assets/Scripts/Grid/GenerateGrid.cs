84422327baf26d01e515eb1d30250e35b7288ebb
darth343 <darth343@gmail.com>
2017-02-14 13:45:01 +0800

    Implemented GridMesh with Terrain

--- StudioProject4_AE_KH_YX_ST/Assets/Scripts/Grid/GenerateGrid.cs
+++ StudioProject4_AE_KH_YX_ST/Assets/Scripts/Grid/GenerateGrid.cs
@@ -2,25 +2,39 @@
 using System.Collections;
 
 public class GenerateGrid : MonoBehaviour {
-    public GameObject m_startingPlane;
-    public int m_width;
-    public int m_height;
-    public const int m_rows = 100;
-    public const int m_columns = 100;
-    private GameObject[,] grid = new GameObject[m_rows, m_columns]; // hardcoded, not sure if const var can be changed in editor , change this if you don't want to hardcode Arun
+    public GameObject StartingGrid;
+    public Terrain ground;
+    public int GridSizeX = 10;
+    public int GridSizeZ = 10;
+    public bool isGenerated = false;
+    private int m_rows;
+    private int m_columns;
+    private GameObject[,] gridmesh;
     // Initialise before game starts or just when game is about to start
     void Awake()
     {
+        if (isGenerated)
+            return;
+        int m_rows = (int)ground.terrainData.size.x / GridSizeX;
+        int m_columns = (int)ground.terrainData.size.z / GridSizeZ;
+        gridmesh = new GameObject[m_rows, m_columns]; // hardcoded, not sure if const var can be changed in editor , change this if you don't want to hardcode Arun
         Debug.Log("run");
         // Create rows
-        for (int x = 0; x < m_width; x += 10)
+        for (int x = 0; x < m_rows; x++)
         { // Create columns
-            for (int z = 0; z < m_height; z += 10)
+            for (int z = 0; z < m_columns; z++)
             {
                 // Create a copy of the plane and offset it according to [current width, current column] using Instantiate
-                GameObject create_plane = (GameObject)Instantiate(m_startingPlane, new Vector3(m_startingPlane.transform.position.x + x, m_startingPlane.transform.position.y, m_startingPlane.transform.position.z + z), m_startingPlane.transform.rotation);
-                grid[x, z] = create_plane;
-
+                GameObject grid = (GameObject)Instantiate(StartingGrid);
+                grid.name = "Row: " + x + " Col: " + z;
+                grid.transform.position = new Vector3(x * GridSizeX + GridSizeX * 0.5f, 0.8f, z * GridSizeZ + GridSizeZ * 0.5f);
+                grid.transform.localScale = new Vector3(GridSizeX, GridSizeZ, 1);
+                grid.transform.SetParent(gameObject.transform);
+                grid.GetComponent<Grid>().position.x = x;
+                grid.GetComponent<Grid>().position.y = z;
+                grid.GetComponent<Grid>().UpdateAvailability();
+                //, new Vector3(m_startingPlane.transform.position.x + x, m_startingPlane.transform.position.y, m_startingPlane.transform.position.z + z), m_startingPlane.transform.rotation);
+                gridmesh[x, z] = grid;
             }
         }
     }
@@ -39,7 +53,7 @@ public class GenerateGrid : MonoBehaviour {
     {
         if (GUI.Button(new Rect(10, 10, 150, 100), "Delete grid[3, 3]"))
         {
-            Destroy(grid[3, 3]); // hardcoded
+            Destroy(gridmesh[3, 3]); // hardcoded
         }
     }
 }
---
+++ StudioProject4_AE_KH_YX_ST/Assets/Scripts/Grid/Grid.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Grid : MonoBehaviour
+{
+    public Texture2D Avail;
+    public Texture2D Unavail;
+    public bool isAvailable = true;
+    public Vector2 position;
+
+    public void UpdateAvailability()
+    {
+        if (0.05 < transform.parent.GetComponent<GenerateGrid>().ground.SampleHeight(new Vector3(position.x * transform.parent.GetComponent<GenerateGrid>().GridSizeX, 0, position.y * transform.parent.GetComponent<GenerateGrid>().GridSizeZ)))
+        {
+            isAvailable = false;
+        }
+        if (!isAvailable)
+            GetComponent<Renderer>().material.SetTexture("_MainTex", Unavail);
+        else
+            GetComponent<Renderer>().material.SetTexture("_MainTex", Avail);
+
+        //GetComponent<Renderer>().material.SetFloat("_Mode", 1);
+    }
+
+	// Use this for initialization
+	void Start () 
+    {
+        //UpdateAvailability();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
